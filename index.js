3eac5cd822f46ee4621a87e26afece871e797428
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2023-09-13 09:54:16 +0300

    start implementing configuration logic

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ try {
 	console.log(`Hello ${nameToGreet}`);
 	const time = (new Date()).toTimeString();
 	core.setOutput("time", time);
+	core.exportVariable("DOCKER_HOST", "ssh://username@host:port")
 
 	const payload = JSON.stringify(github.context.payload, undefined, 2)
 	console.log(payload)
